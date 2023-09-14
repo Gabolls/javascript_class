f224a890ff3d3371790a1d40b7145f746891b721
gabolls <gabolls2101@gmail.com>
2023-09-14 14:11:49 -0400

    arregle los archivos js, faltan estilos

---
+++ src/main.js
@@ -0,0 +1,48 @@
+import "./style.css"
+import { CardSong } from "./components/CardSong"
+import { reloadButtonPlay, setCurrentSong } from "./components/CurrentSong"
+import { getData } from "./services"
+
+
+const track_list = document.getElementById("track_list")
+
+getData(data => {
+  data.map(song => {
+    track_list?.appendChild(
+      CardSong({
+        data: song,
+        eventPlay: () => {
+          setCurrentSong(song)
+          reloadButtonPlay()
+        },
+        eventLike: () => console.log("Like")
+      })
+    )
+  })
+})
+
+
+
+
+
+
+
+
+
+// elementoDOM.appendChild(nuevoElemento)
+
+
+
+// console.log(elementoDOM)
+
+
+
+
+
+
+
+
+
+
+
+
